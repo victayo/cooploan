bd6ef9c8d5063de39211791cc203d503c48ca176
victokala <victokala@gmail.com>
2023-10-23 14:11:01 +0100

    Created employment details model and migration

---
+++ database/migrations/2023_10_23_125417_create_employment_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employment_details', function (Blueprint $table) {
+            $table->string('mainone_id');
+            $table->string('department');
+            $table->date('resumption_date');
+            $table->string('job_title');
+            $table->timestamps();
+            $table->foreign('mainone_id')->references('mainone_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employment_details');
+    }
+};
