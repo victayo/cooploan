7cf4479b4a3bab3662b7b4fa6c503df0affd42e6
victokala <victokala@gmail.com>
2023-11-08 05:57:03 +0100

    Created Tenure db seeder

---
+++ database/seeders/TenureSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tenure;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TenureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for($i = 2; $i <= 36; $i++){
+            if($i >= 2 && $i <= 18){ // 2 - 18 months, interest is 10%
+                $interest = 10;
+            }else if($i > 18 && $i <= 24){// 19 - 24 months, interest is 12%
+                $interest = 12;
+            }else{ // 25 - 36 months, interest is 15%
+                $interest = 15;
+            }
+
+            Tenure::create([
+                'tenure' => $i,
+                'interest' => $interest
+            ]);
+        }
+
+
+    }
+}
