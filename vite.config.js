16f4d85caab57de240bfef5340a29d8e379c565b
victokala <victokala@gmail.com>
2024-03-02 18:09:37 +0100

    reference vite port from .env variable

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,39 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    server: {
-        host: true,
-        hmr: {
-            host: 'localhost',
-        },
-        port: 5173
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/scss/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+    return defineConfig({
+        server: {
+            host: true,
+            hmr: {
+                host: 'localhost',
             },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+            port: parseInt(process.env.VITE_PORT)
         },
-    },
-});
+        plugins: [
+            laravel({
+                input: [
+                    'resources/scss/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm-bundler.js',
+            },
+        },
+    });
+}
